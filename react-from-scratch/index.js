682cfb8570041ec99c5b102ff231f72bf7a93db3
Jayden Houghton <jayden.hou12@gmail.com>
2025-08-24 12:35:16 +1200

    Revert "Install packages, simple rendering with Webpack and Babel"
    This reverts commit 33ec8f37e502bdee5a55bebcb99c55093011bb2a.

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -9,4 +9,5 @@ const App = () => {
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<App />);
+/* root.render(<App />); */
+root.render(React.createElement(App));
