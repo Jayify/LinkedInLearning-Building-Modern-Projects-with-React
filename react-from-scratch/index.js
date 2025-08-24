3c90e83c62aa3fc696ccdbabc9c7cf106b93771b
Jayden Houghton <jayden.hou12@gmail.com>
2025-08-24 12:12:27 +1200

    Write basic render script with vanilla React handling

--- react-from-scratch/index.js
+++ react-from-scratch/index.js
@@ -1,1 +1,13 @@
-console.log("Started React render script");
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const App = () => {
+    /* return <h1>This is a React script.</h1>*/
+    return React.createElement('h1', null, 'This is a React script.');
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+/* root.render(<App />); */
+root.render(React.createElement(App));
